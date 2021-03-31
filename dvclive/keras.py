f56f43c6bb97cb7655e1cd4594cfbd531e508907
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2021-03-31 02:03:39 -0700

    Save checkpoint for Keras

--- dvclive/keras.py
+++ dvclive/keras.py
@@ -4,8 +4,13 @@ import dvclive
 
 
 class DvcLiveCallback(Callback):
+    def __init__(self, model_file = None):
+        self.model_file = model_file
+
     def on_epoch_end(self, epoch: int, logs: dict = None):
         logs = logs or {}
         for metric, value in logs.items():
             dvclive.log(metric, value)
+        if self.model_file:
+            self.model.save(self.model_file)
         dvclive.next_step()
