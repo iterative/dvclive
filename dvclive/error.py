f13152213d46f5b6555d6a50df436cd5b094539d
ssamal <satyaranjan.samal@lowes.com>
2021-06-16 19:11:20 +0530

    refactored error message

--- dvclive/error.py
+++ dvclive/error.py
@@ -5,6 +5,5 @@ class DvcLiveError(Exception):
 class InitializationError(DvcLiveError):
     def __init__(self):
         super().__init__(
-            "Initialization error - call `dvclive.init()` before "
-            "`dvclive.log()`"
+            "Initialization error - no call was made to `dvclive.init()` or `dvclive.log()` "
         )
