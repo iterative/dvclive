bbb59fd8ee0f065d6d49d01fb5aaf42fccbe089c
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-06-17 11:27:46 +0200

    Remove todo

--- tests/test_main.py
+++ tests/test_main.py
@@ -97,7 +97,6 @@ def test_clean_up(tmp_dir):
     dvclive.init("logs", summary=True, html=True)
     dvclive.log("m1", 1)
     dvclive.next_step()
-    # TODO: how to properly mock dvc html creation
     (tmp_dir / "logs.html").touch()
 
     assert (tmp_dir / "logs" / "m1.tsv").is_file()
