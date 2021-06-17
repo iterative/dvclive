2e43f5c51c545aa36b57fb1b2d90dce73bcd603f
ssamal <satyaranjan.samal@lowes.com>
2021-06-17 16:47:32 +0530

    formatting the message

--- dvclive/error.py
+++ dvclive/error.py
@@ -5,5 +5,6 @@ class DvcLiveError(Exception):
 class InitializationError(DvcLiveError):
     def __init__(self):
         super().__init__(
-            "Initialization error - no call was made to `dvclive.init()` or `dvclive.log()` "
+            "Initialization error - no call was made to `dvclive.init()` "
+            " or `dvclive.log()` "
         )
