280800a8a8a644ce6ca465b92c7d6123a8cf97ae
Paweł Redzyński <pawelredzynski@gmail.com>
2021-07-29 11:31:02 +0200

    init: throw on dvc/dvclive config mismatch (#74)
    * logger: throw on config mismatch
    
    * daavo review fixups
    
    * fixup

--- dvclive/error.py
+++ dvclive/error.py
@@ -1,3 +1,10 @@
+import os
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .metrics import MetricLogger
+
+
 class DvcLiveError(Exception):
     pass
 
@@ -7,4 +14,14 @@ class InitializationError(DvcLiveError):
         super().__init__(
             "Initialization error - no call was made to `dvclive.init()` "
             " or `dvclive.log()` "
+        )
+
+
+class ConfigMismatchError(DvcLiveError):
+    def __init__(self, ml: "MetricLogger"):
+        from . import env
+
+        super().__init__(
+            f"Dvclive initialized in '{ml.dir}' conflicts "
+            f"with '{os.environ[env.DVCLIVE_PATH]}' provided by DVC."
         )
