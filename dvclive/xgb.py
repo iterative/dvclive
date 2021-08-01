77d22be488a3736e1d4fdc4c1837bb950f7277eb
Ezhova Darya <38072625+ezhdi@users.noreply.github.com>
2021-08-01 22:48:13 +0400

    Update xgb.py
    Add save model

--- dvclive/xgb.py
+++ dvclive/xgb.py
@@ -4,13 +4,16 @@ import dvclive
 
 
 class DvcLiveCallback(TrainingCallback):
-    def __init__(self, metric_data):
+    def __init__(self, metric_data, model_file=None):
         super().__init__()
         self._metric_data = metric_data
+        self.model_file = model_file
 
     def after_iteration(self, model, epoch, evals_log):
         for key, values in evals_log[self._metric_data].items():
             if values:
                 latest_metric = values[-1]
             dvclive.log(key, latest_metric)
+        if self.model_file:
+            model.save_model(self.model_file)
         dvclive.next_step()
