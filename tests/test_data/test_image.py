979c55ddda0981b6d55c98987d9aeb6656d87f21
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-09-20 21:02:18 +0200

    pre-commit

--- tests/test_data/test_image.py
+++ tests/test_data/test_image.py
@@ -10,17 +10,13 @@ from dvclive.data import ImageNumpy, ImagePIL
 
 def test_PIL(tmp_dir):
     logger = dvclive.init()
-    img = Image.new('RGB', (500,500), (250,250,250))
+    img = Image.new("RGB", (500, 500), (250, 250, 250))
     dvclive.log("image.png", img)
 
     assert (tmp_dir / logger.dir / ImagePIL.subdir / "image.png").exists()
 
 
-@pytest.mark.parametrize("shape", [
-    (500, 500),
-    (500, 500, 3),
-    (500, 500, 4)
-])
+@pytest.mark.parametrize("shape", [(500, 500), (500, 500, 3), (500, 500, 4)])
 def test_numpy(tmp_dir, shape):
     logger = dvclive.init()
     img = np.ones(shape, np.uint8) * 255
@@ -29,10 +25,9 @@ def test_numpy(tmp_dir, shape):
     assert (tmp_dir / logger.dir / ImageNumpy.subdir / "image.png").exists()
 
 
-@pytest.mark.parametrize("pattern", [
-    "image_{step}.png", 
-    str(os.path.join("{step}", "image.png"))
-])
+@pytest.mark.parametrize(
+    "pattern", ["image_{step}.png", str(os.path.join("{step}", "image.png"))]
+)
 def test_step_formatting(tmp_dir, pattern):
     logger = dvclive.init()
     img = np.ones((500, 500, 3), np.uint8)
@@ -41,4 +36,6 @@ def test_step_formatting(tmp_dir, pattern):
         dvclive.next_step()
 
     for step in range(3):
-        assert (tmp_dir / logger.dir / ImagePIL.subdir / pattern.format(step=step)).exists()
+        assert (
+            tmp_dir / logger.dir / ImagePIL.subdir / pattern.format(step=step)
+        ).exists()
