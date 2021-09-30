8362e54e9bbb5aa67b2c8d36a43c323ddcd7b137
sirily <ilsir95@gmail.com>
2021-09-30 20:34:17 +0300

    Add new callback to DVCLive

---
+++ dvclive/lightning.py
@@ -0,0 +1,80 @@
+from typing import Dict, Optional, Union
+
+from pytorch_lightning.loggers import LightningLoggerBase
+from pytorch_lightning.loggers.base import rank_zero_experiment
+from pytorch_lightning.utilities import rank_zero_only
+from torch import is_tensor
+
+import dvclive
+
+
+class DvcLiveLogger(LightningLoggerBase):
+    def __init__(
+        self,
+        run_name: Union[str, int],
+        save_dir: Optional[str] = None,
+        summary: Optional[bool] = True,
+        prefix="",
+        experiment=None,
+    ):
+
+        if dvclive is None:
+            raise ImportError(
+                "You want to use `dvclive` logger which is not installed yet,"
+                " install it with `pip install dvclive`."
+            )
+
+        super().__init__()
+        self._prefix = prefix
+        self._dvclive_init = dict(
+            path=save_dir,
+            summary=summary,
+        )
+        self._experiment = experiment
+        self._version = run_name
+
+    @property
+    def name(self):
+        return "DvcLiveLogger"
+
+    @rank_zero_only
+    def log_hyperparams(self, params, *args, **kwargs):
+        pass
+
+    @property
+    @rank_zero_experiment
+    def experiment(self):
+        r"""
+        Actual DVCLive object. To use DVCLive features in your
+        :class:`~LightningModule` do the following.
+        Example::
+            self.logger.experiment.some_dvclive_function()
+        """
+        if self._experiment is not None:
+            return self._experiment
+        else:
+            assert (
+                rank_zero_only.rank == 0
+            ), "tried to init log dirs in non global_rank=0"
+            self._experiment = dvclive.init(**self._dvclive_init)
+
+        return self._experiment
+
+    @property
+    def version(self):
+        return self._version
+
+    @rank_zero_only
+    def log_metrics(
+        self, metrics: Dict[str, float], step: Optional[int] = None
+    ):
+        assert (
+            rank_zero_only.rank == 0
+        ), "experiment tried to log from global_rank != 0"
+
+        metrics = self._add_prefix(metrics)
+        for metric_name, metric_val in metrics.items():
+            if is_tensor(metric_val):
+                metric_val = metric_val.cpu().detach().item()
+            self.experiment.log(name=metric_name, val=metric_val)
+        self.experiment.next_step()
