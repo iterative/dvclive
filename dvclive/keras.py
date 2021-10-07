bced1a5fb3a92092d24537cdd84e904017e554d0
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-10-07 20:13:54 +0200

    Added keras model loading on resume

--- dvclive/keras.py
+++ dvclive/keras.py
@@ -1,6 +1,9 @@
+import os
+
 from tensorflow.keras.callbacks import (  # pylint: disable=no-name-in-module
     Callback,
 )
+from tensorflow.keras.models import load_model  # pylint: disable=no-name-in-module
 
 from dvclive import Live
 
@@ -13,6 +16,17 @@ class DvcLiveCallback(Callback):
         self.model_file = model_file
         self.save_weights_only = save_weights_only
         self.dvclive = Live(**kwargs)
+
+    def on_train_begin(self, logs=None):
+        if (
+            self.dvclive._resume and
+            self.model_file is not None
+            and os.path.exists(self.model_file)
+        ):
+            if self.save_weights_only:
+                self.model.load_weights(self.model_file)
+            else:
+                self.model = load_model(self.model_file)
 
     def on_epoch_end(self, epoch: int, logs: dict = None):
         logs = logs or {}
