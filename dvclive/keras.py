f60212d07c0fd5542227f2e13f42be51248388f1
Paweł Redzyński <pawelredzynski@gmail.com>
2022-02-14 13:51:27 +0100

    pre-commit: fix keras module

--- dvclive/keras.py
+++ dvclive/keras.py
@@ -1,9 +1,9 @@
 import os
 
-from tensorflow.keras.callbacks import (  # pylint: disable=no-name-in-module
+from tensorflow.keras.callbacks import (  # noqa pylint: disable=import-error, no-name-in-module
     Callback,
 )
-from tensorflow.keras.models import (  # pylint: disable=no-name-in-module
+from tensorflow.keras.models import (  # noqa pylint: disable=import-error, no-name-in-module
     load_model,
 )
 
@@ -19,18 +19,24 @@ class DvcLiveCallback(Callback):
         self.save_weights_only = save_weights_only
         self.dvclive = Live(**kwargs)
 
-    def on_train_begin(self, logs=None):
+    def on_train_begin(self, logs=None):  # pylint: disable=unused-argument
         if (
             self.dvclive._resume
             and self.model_file is not None
             and os.path.exists(self.model_file)
         ):
             if self.save_weights_only:
-                self.model.load_weights(self.model_file)
+                self.model.load_weights(  # noqa pylint: disable=access-member-before-definition
+                    self.model_file
+                )
             else:
-                self.model = load_model(self.model_file)
+                self.model = load_model(  # noqa pylint: disable=attribute-defined-outside-init
+                    self.model_file
+                )
 
-    def on_epoch_end(self, epoch: int, logs: dict = None):
+    def on_epoch_end(
+        self, epoch: int, logs: dict = None
+    ):  # pylint: disable=unused-argument
         logs = logs or {}
         for metric, value in logs.items():
             self.dvclive.log(metric, value)
