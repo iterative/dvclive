086659f634baf9102583b2b29e2fc5903cf9e12b
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-08-23 13:42:41 +0200

    tests: Remove hardcoded name (#275)

--- tests/test_data/test_scalar.py
+++ tests/test_data/test_scalar.py
@@ -5,6 +5,7 @@ import pytest
 
 # pylint: disable=unused-argument
 from dvclive import Live
+from dvclive.data.scalar import Scalar
 from dvclive.data.utils import NUMPY_INTS, NUMPY_SCALARS
 from dvclive.utils import parse_tsv
 
@@ -19,6 +20,6 @@ def test_numpy(tmp_dir, dtype):
 
     parsed = json.loads((tmp_dir / live.summary_path).read_text())
     assert isinstance(parsed["scalar"], int if dtype in NUMPY_INTS else float)
-    tsv_file = tmp_dir / live.dir / "scalars" / "scalar.tsv"
+    tsv_file = tmp_dir / live.dir / Scalar.subfolder / "scalar.tsv"
     tsv_val = parse_tsv(tsv_file)[0]["scalar"]
     assert tsv_val == str(scalar)
