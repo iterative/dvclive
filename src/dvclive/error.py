27a6c0c4a06ae85fee1a833610e12cee50bc896b
Daniele Trifirò <daniele@iterative.ai>
2022-09-06 14:01:49 +0200

    add log_param/log_params

--- src/dvclive/error.py
+++ src/dvclive/error.py
@@ -1,5 +1,5 @@
 import os
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Optional
 
 if TYPE_CHECKING:
     from .live import Live
@@ -44,3 +44,27 @@ class DataAlreadyLoggedError(DvcLiveError):
         super().__init__(
             f"Data '{name}' has already been logged with step '{step}'"
         )
+
+
+class ParameterAlreadyLoggedError(DvcLiveError):
+    def __init__(
+        self, name: str, val: Any, previous_val: Optional[Any] = None
+    ):
+        self.name = name
+        self.val = val
+        self.previous_val = previous_val
+        super().__init__(
+            f"Parameter '{name}={val}' has already been logged"
+            + (
+                f" (previous value={self.previous_val})."
+                if self.previous_val is not None
+                and self.val != self.previous_val
+                else "."
+            )
+        )
+
+
+class InvalidParameterTypeError(DvcLiveError):
+    def __init__(self, val: Any):
+        self.val = val
+        super().__init__(f"Parameter type {type(val)} is not supported.")
