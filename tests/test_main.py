781ba647ddfbbe7dcb4c16b6dd7fb8c84f783d4f
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-12-07 15:12:50 +0100

    live.end: Don't increment `step` value.
    Add `update_step` option to `make_summary`, default `True` to preserve old behavior but set to `False` inside `live.end`.
    
    Closes #389

--- tests/test_main.py
+++ tests/test_main.py
@@ -385,6 +385,18 @@ def test_make_summary_is_called_on_end(tmp_dir):
     assert not log_file.exists()
 
 
+def test_make_summary_on_end_dont_increment_step(tmp_dir):
+    with Live() as live:
+        for i in range(2):
+            live.log_metric("foo", i)
+            live.next_step()
+
+    assert json.loads((tmp_dir / live.metrics_file).read_text()) == {
+        "foo": 1.0,
+        "step": 1,
+    }
+
+
 def test_context_manager(tmp_dir):
     with Live() as live:
         live.summary["foo"] = 1.0
