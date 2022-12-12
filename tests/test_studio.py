9177c52f42331b67e7a01f33567cd0373a053519
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-12-12 14:01:23 +0100

    studio: Remove `STUDIO_REPO_URL` requirement.
    Closes #368

--- tests/test_studio.py
+++ tests/test_studio.py
@@ -6,6 +6,12 @@ import pytest
 
 from dvclive import Live, env
 from dvclive.plots import Metric
+from dvclive.studio import (
+    VALID_URLS,
+    _convert_to_studio_url,
+    _get_remote_url,
+    get_studio_repo_url,
+)
 
 
 def test_post_to_studio(tmp_dir, mocker, monkeypatch):
@@ -200,3 +206,71 @@ def test_post_to_studio_skip_on_env_var(tmp_dir, mocker, monkeypatch):
         live.next_step()
 
     assert mocked_post.call_count == 1
+
+
+@pytest.mark.studio
+@pytest.mark.parametrize(
+    "url,expected",
+    [
+        (
+            "https://github.com/USERNAME/REPOSITORY.git",
+            "github:USERNAME/REPOSITORY",
+        ),
+        (
+            "https://gitlab.com/USERNAME/REPOSITORY",
+            "gitlab:USERNAME/REPOSITORY",
+        ),
+        (
+            "https://bitbucket.org/USERNAME/REPOSITORY",
+            "bitbucket:USERNAME/REPOSITORY",
+        ),
+        (
+            "git@github.com:USERNAME/REPOSITORY.git",
+            "github:USERNAME/REPOSITORY",
+        ),
+        (
+            "git@gitlab.com:USERNAME/REPOSITORY.git",
+            "gitlab:USERNAME/REPOSITORY",
+        ),
+        (
+            "git@bitbucket.org:USERNAME/REPOSITORY.git",
+            "bitbucket:USERNAME/REPOSITORY",
+        ),
+    ],
+)
+def test_convert_to_studio_url(url, expected, mocker):
+    assert _convert_to_studio_url(url) == expected
+
+
+def test_get_remote_url(tmp_dir):
+    from git import Repo
+
+    repo = Repo.clone_from("https://github.com/iterative/dvclive.git", tmp_dir)
+    assert _get_remote_url(repo) == "https://github.com/iterative/dvclive.git"
+
+
+def test_get_studio_repo_url_warnings(caplog, mocker):
+    from git.exc import GitError
+
+    mocker.patch("dvclive.studio._get_remote_url", side_effect=GitError())
+    caplog.clear()
+    get_studio_repo_url(None)
+    assert caplog.records[0].message == (
+        "Tried to find remote url for the active branch but failed.\n"
+    )
+    assert caplog.records[1].message == (
+        "You can try manually setting the Studio repo url using the"
+        " environment variable `STUDIO_REPO_URL`."
+    )
+
+    mocker.patch("dvclive.studio._get_remote_url", return_value="bad@repo:url")
+    caplog.clear()
+    get_studio_repo_url(None)
+    assert caplog.records[0].message == (
+        "Found invalid remote url for the active branch.\n"
+        f" Supported urls must start with any of {VALID_URLS}"
+    )
+    assert caplog.records[1].message == (
+        "You can try manually setting the Studio repo url using the"
+        " environment variable `STUDIO_REPO_URL`."
+    )
