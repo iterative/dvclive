986270de5f770193fd198ed2f7c965da087ecc3c
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-12-27 09:39:30 +0100

    Use `dvc-studio-client`.

--- src/dvclive/studio.py
+++ src/dvclive/studio.py
@@ -1,8 +1,6 @@
 # pylint: disable=protected-access
 import logging
-from os import getenv
 
-from dvclive.env import STUDIO_ENDPOINT
 from dvclive.utils import parse_metrics
 
 logger = logging.getLogger(__name__)
@@ -31,9 +29,9 @@ def _to_dvc_format(plots):
     return formatted
 
 
-def _get_updates(live):
+def get_studio_updates(live):
     plots, metrics = parse_metrics(live)
-    latest_step = live._latest_studio_step  # pylint: disable=protected-access
+    latest_step = live._latest_studio_step
 
     for name, plot in plots.items():
         datapoints = _get_unsent_datapoints(plot, latest_step)
@@ -42,98 +40,3 @@ def _get_updates(live):
     metrics = {live.metrics_file: {"data": metrics}}
     plots = _to_dvc_format(plots)
     return metrics, plots
-
-
-def post_to_studio(live, event_type) -> bool:
-    import requests
-    from requests.exceptions import RequestException
-
-    data = {
-        "type": event_type,
-        "repo_url": live._studio_url,
-        "rev": live._baseline_rev,
-        "client": "dvclive",
-    }
-
-    if event_type == "data":
-        metrics, plots = _get_updates(live)
-        data["metrics"] = metrics
-        data["plots"] = plots
-        data["step"] = live.step
-
-    logger.debug(f"post_to_studio `{event_type=}`")
-
-    try:
-        response = requests.post(
-            getenv(STUDIO_ENDPOINT, "https://studio.iterative.ai/api/live"),
-            json=data,
-            headers={
-                "Content-type": "application/json",
-                "Authorization": f"token {live._studio_token}",
-            },
-            timeout=5,
-        )
-    except RequestException:
-        return False
-
-    message = response.content.decode()
-    logger.debug(
-        f"post_to_studio: {response.status_code=}" f", {message=}"
-        if message
-        else ""
-    )
-
-    return response.status_code == 200
-
-
-def _get_remote_url(git_repo):
-    return git_repo.git.ls_remote("--get-url")
-
-
-VALID_PREFIXES = ("https://", "git@")
-VALID_PROVIDERS = ("github.com", "gitlab.com", "bitbucket.org")
-VALID_URLS = [
-    f"{prefix}{provider}"
-    for prefix in VALID_PREFIXES
-    for provider in VALID_PROVIDERS
-]
-
-
-def _convert_to_studio_url(remote_url):
-    studio_url = ""
-    for prefix in VALID_PREFIXES:
-        for provider in VALID_PROVIDERS:
-            if remote_url.startswith(f"{prefix}{provider}"):
-                repo = remote_url.split(provider)[-1]
-                repo = repo.rstrip(".git")
-                repo = repo.lstrip("/")
-                repo = repo.lstrip(":")
-                studio_url = f"{provider.split('.')[0]}:{repo}"
-    if not studio_url:
-        raise ValueError
-    return studio_url
-
-
-def get_studio_repo_url(git_repo) -> str:
-    from git.exc import GitError
-
-    studio_url = ""
-    try:
-        remote_url = _get_remote_url(git_repo)
-        studio_url = _convert_to_studio_url(remote_url)
-    except GitError:
-        logger.warning(
-            "Tried to find remote url for the active branch but failed.\n"
-        )
-    except ValueError:
-        logger.warning(
-            "Found invalid remote url for the active branch.\n"
-            f" Supported urls must start with any of {VALID_URLS}"
-        )
-    finally:
-        if not studio_url:
-            logger.warning(
-                "You can try manually setting the Studio repo url using the"
-                " environment variable `STUDIO_REPO_URL`."
-            )
-        return studio_url  # noqa: B012  # pylint:disable=lost-exception
--- tests/test_studio.py
+++ tests/test_studio.py
@@ -3,27 +3,27 @@
 import os
 
 import pytest
-
-from dvclive import Live, env
-from dvclive.plots import Metric
-from dvclive.studio import (
-    VALID_URLS,
-    _convert_to_studio_url,
-    _get_remote_url,
-    get_studio_repo_url,
+from dvc_studio_client.env import (
+    STUDIO_ENDPOINT,
+    STUDIO_REPO_URL,
+    STUDIO_TOKEN,
 )
+
+from dvclive import Live
+from dvclive.env import DVC_EXP_BASELINE_REV, DVC_EXP_NAME
+from dvclive.plots import Metric
 
 
 def test_post_to_studio(tmp_dir, mocker, monkeypatch):
     dvc_repo = mocker.MagicMock()
-    dvc_repo.scm.get_rev.return_value = "current_rev"
+    dvc_repo.scm.get_rev.return_value = "f" * 40
     mocker.patch("dvclive.live.get_dvc_repo", return_value=dvc_repo)
     mocked_response = mocker.MagicMock()
     mocked_response.status_code = 200
     mocked_post = mocker.patch("requests.post", return_value=mocked_response)
-    monkeypatch.setenv(env.STUDIO_ENDPOINT, "https://0.0.0.0")
-    monkeypatch.setenv(env.STUDIO_REPO_URL, "STUDIO_REPO_URL")
-    monkeypatch.setenv(env.STUDIO_TOKEN, "STUDIO_TOKEN")
+    monkeypatch.setenv(STUDIO_ENDPOINT, "https://0.0.0.0")
+    monkeypatch.setenv(STUDIO_REPO_URL, "STUDIO_REPO_URL")
+    monkeypatch.setenv(STUDIO_TOKEN, "STUDIO_TOKEN")
 
     live = Live()
 
@@ -34,7 +34,8 @@ def test_post_to_studio(tmp_dir, mocker, monkeypatch):
         json={
             "type": "start",
             "repo_url": "STUDIO_REPO_URL",
-            "rev": "current_rev",
+            "baseline_sha": "f" * 40,
+            "name": "dvclive-exp",
             "client": "dvclive",
         },
         headers={
@@ -52,7 +53,8 @@ def test_post_to_studio(tmp_dir, mocker, monkeypatch):
         json={
             "type": "data",
             "repo_url": "STUDIO_REPO_URL",
-            "rev": "current_rev",
+            "baseline_sha": "f" * 40,
+            "name": "dvclive-exp",
             "step": 0,
             "metrics": {live.metrics_file: {"data": {"step": 0, "foo": 1}}},
             "plots": {scalar_path: {"data": [{"step": 0, "foo": 1.0}]}},
@@ -73,7 +75,8 @@ def test_post_to_studio(tmp_dir, mocker, monkeypatch):
         json={
             "type": "data",
             "repo_url": "STUDIO_REPO_URL",
-            "rev": "current_rev",
+            "baseline_sha": "f" * 40,
+            "name": "dvclive-exp",
             "step": 1,
             "metrics": {live.metrics_file: {"data": {"step": 1, "foo": 2}}},
             "plots": {scalar_path: {"data": [{"step": 1, "foo": 2.0}]}},
@@ -92,7 +95,8 @@ def test_post_to_studio(tmp_dir, mocker, monkeypatch):
         json={
             "type": "done",
             "repo_url": "STUDIO_REPO_URL",
-            "rev": mocker.ANY,
+            "baseline_sha": "f" * 40,
+            "name": "dvclive-exp",
             "client": "dvclive",
         },
         headers={
@@ -104,13 +108,15 @@ def test_post_to_studio(tmp_dir, mocker, monkeypatch):
 
 
 def test_post_to_studio_failed_data_request(tmp_dir, mocker, monkeypatch):
-    mocker.patch("dvclive.live.get_dvc_repo")
+    dvc_repo = mocker.MagicMock()
+    dvc_repo.scm.get_rev.return_value = "f" * 40
+    mocker.patch("dvclive.live.get_dvc_repo", return_value=dvc_repo)
     valid_response = mocker.MagicMock()
     valid_response.status_code = 200
     mocker.patch("requests.post", return_value=valid_response)
-    monkeypatch.setenv(env.STUDIO_ENDPOINT, "https://0.0.0.0")
-    monkeypatch.setenv(env.STUDIO_REPO_URL, "STUDIO_REPO_URL")
-    monkeypatch.setenv(env.STUDIO_TOKEN, "STUDIO_TOKEN")
+    monkeypatch.setenv(STUDIO_ENDPOINT, "https://0.0.0.0")
+    monkeypatch.setenv(STUDIO_REPO_URL, "STUDIO_REPO_URL")
+    monkeypatch.setenv(STUDIO_TOKEN, "STUDIO_TOKEN")
 
     live = Live()
 
@@ -130,7 +136,8 @@ def test_post_to_studio_failed_data_request(tmp_dir, mocker, monkeypatch):
         json={
             "type": "data",
             "repo_url": "STUDIO_REPO_URL",
-            "rev": mocker.ANY,
+            "baseline_sha": "f" * 40,
+            "name": "dvclive-exp",
             "step": 1,
             "metrics": {live.metrics_file: {"data": {"step": 1, "foo": 2}}},
             "plots": {
@@ -152,14 +159,16 @@ def test_post_to_studio_failed_data_request(tmp_dir, mocker, monkeypatch):
 
 
 def test_post_to_studio_failed_start_request(tmp_dir, mocker, monkeypatch):
-    mocker.patch("dvclive.live.get_dvc_repo")
+    dvc_repo = mocker.MagicMock()
+    dvc_repo.scm.get_rev.return_value = "f" * 40
+    mocker.patch("dvclive.live.get_dvc_repo", return_value=dvc_repo)
     mocked_response = mocker.MagicMock()
     mocked_response.status_code = 400
     mocked_post = mocker.patch("requests.post", return_value=mocked_response)
 
-    monkeypatch.setenv(env.STUDIO_ENDPOINT, "https://0.0.0.0")
-    monkeypatch.setenv(env.STUDIO_REPO_URL, "STUDIO_REPO_URL")
-    monkeypatch.setenv(env.STUDIO_TOKEN, "STUDIO_TOKEN")
+    monkeypatch.setenv(STUDIO_ENDPOINT, "https://0.0.0.0")
+    monkeypatch.setenv(STUDIO_REPO_URL, "STUDIO_REPO_URL")
+    monkeypatch.setenv(STUDIO_TOKEN, "STUDIO_TOKEN")
 
     live = Live()
 
@@ -173,13 +182,15 @@ def test_post_to_studio_failed_start_request(tmp_dir, mocker, monkeypatch):
 
 
 def test_post_to_studio_end_only_once(tmp_dir, mocker, monkeypatch):
-    mocker.patch("dvclive.live.get_dvc_repo")
+    dvc_repo = mocker.MagicMock()
+    dvc_repo.scm.get_rev.return_value = "f" * 40
+    mocker.patch("dvclive.live.get_dvc_repo", return_value=dvc_repo)
     valid_response = mocker.MagicMock()
     valid_response.status_code = 200
     mocked_post = mocker.patch("requests.post", return_value=valid_response)
-    monkeypatch.setenv(env.STUDIO_ENDPOINT, "https://0.0.0.0")
-    monkeypatch.setenv(env.STUDIO_REPO_URL, "STUDIO_REPO_URL")
-    monkeypatch.setenv(env.STUDIO_TOKEN, "STUDIO_TOKEN")
+    monkeypatch.setenv(STUDIO_ENDPOINT, "https://0.0.0.0")
+    monkeypatch.setenv(STUDIO_REPO_URL, "STUDIO_REPO_URL")
+    monkeypatch.setenv(STUDIO_TOKEN, "STUDIO_TOKEN")
 
     with Live() as live:
         live.log_metric("foo", 1)
@@ -192,87 +203,21 @@ def test_post_to_studio_end_only_once(tmp_dir, mocker, monkeypatch):
 
 @pytest.mark.studio
 def test_post_to_studio_skip_on_env_var(tmp_dir, mocker, monkeypatch):
-    mocker.patch("dvclive.live.get_dvc_repo")
+    dvc_repo = mocker.MagicMock()
+    dvc_repo.scm.get_rev.return_value = "f" * 40
+    mocker.patch("dvclive.live.get_dvc_repo", return_value=dvc_repo)
     valid_response = mocker.MagicMock()
     valid_response.status_code = 200
     mocked_post = mocker.patch("requests.post", return_value=valid_response)
-    monkeypatch.setenv(env.STUDIO_ENDPOINT, "https://0.0.0.0")
-    monkeypatch.setenv(env.STUDIO_REPO_URL, "STUDIO_REPO_URL")
-    monkeypatch.setenv(env.STUDIO_TOKEN, "STUDIO_TOKEN")
+    monkeypatch.setenv(STUDIO_ENDPOINT, "https://0.0.0.0")
+    monkeypatch.setenv(STUDIO_REPO_URL, "STUDIO_REPO_URL")
+    monkeypatch.setenv(STUDIO_TOKEN, "STUDIO_TOKEN")
 
-    monkeypatch.setenv(env.DVC_EXP_BASELINE_REV, "foo")
-    monkeypatch.setenv(env.DVC_EXP_NAME, "bar")
+    monkeypatch.setenv(DVC_EXP_BASELINE_REV, "foo")
+    monkeypatch.setenv(DVC_EXP_NAME, "bar")
 
     with Live() as live:
         live.log_metric("foo", 1)
         live.next_step()
 
-    assert mocked_post.call_count == 1
-
-
-@pytest.mark.studio
-@pytest.mark.parametrize(
-    "url,expected",
-    [
-        (
-            "https://github.com/USERNAME/REPOSITORY.git",
-            "github:USERNAME/REPOSITORY",
-        ),
-        (
-            "https://gitlab.com/USERNAME/REPOSITORY",
-            "gitlab:USERNAME/REPOSITORY",
-        ),
-        (
-            "https://bitbucket.org/USERNAME/REPOSITORY",
-            "bitbucket:USERNAME/REPOSITORY",
-        ),
-        (
-            "git@github.com:USERNAME/REPOSITORY.git",
-            "github:USERNAME/REPOSITORY",
-        ),
-        (
-            "git@gitlab.com:USERNAME/REPOSITORY.git",
-            "gitlab:USERNAME/REPOSITORY",
-        ),
-        (
-            "git@bitbucket.org:USERNAME/REPOSITORY.git",
-            "bitbucket:USERNAME/REPOSITORY",
-        ),
-    ],
-)
-def test_convert_to_studio_url(url, expected, mocker):
-    assert _convert_to_studio_url(url) == expected
-
-
-def test_get_remote_url(tmp_dir):
-    from git import Repo
-
-    repo = Repo.clone_from("https://github.com/iterative/dvclive.git", tmp_dir)
-    assert _get_remote_url(repo) == "https://github.com/iterative/dvclive.git"
-
-
-def test_get_studio_repo_url_warnings(caplog, mocker):
-    from git.exc import GitError
-
-    mocker.patch("dvclive.studio._get_remote_url", side_effect=GitError())
-    caplog.clear()
-    get_studio_repo_url(None)
-    assert caplog.records[0].message == (
-        "Tried to find remote url for the active branch but failed.\n"
-    )
-    assert caplog.records[1].message == (
-        "You can try manually setting the Studio repo url using the"
-        " environment variable `STUDIO_REPO_URL`."
-    )
-
-    mocker.patch("dvclive.studio._get_remote_url", return_value="bad@repo:url")
-    caplog.clear()
-    get_studio_repo_url(None)
-    assert caplog.records[0].message == (
-        "Found invalid remote url for the active branch.\n"
-        f" Supported urls must start with any of {VALID_URLS}"
-    )
-    assert caplog.records[1].message == (
-        "You can try manually setting the Studio repo url using the"
-        " environment variable `STUDIO_REPO_URL`."
-    )
+    assert mocked_post.call_count == 0
