95331461b8b94459e50a4047be9a28a37ef6ea89
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2023-03-20 11:33:39 +0100

    Optuna integration.
    Use `save_dvc_exp` to create a DVC experiment for each trial.

---
+++ tests/test_frameworks/test_optuna.py
@@ -0,0 +1,27 @@
+# pylint: disable=protected-access, unused-argument
+import optuna
+
+from dvclive.optuna import DVCLiveCallback
+from dvclive.serialize import load_yaml
+from dvclive.utils import parse_json
+
+
+def objective(trial):
+    x = trial.suggest_float("x", -10, 10)
+    return (x - 2) ** 2
+
+
+def test_optuna_(tmp_dir, mocked_dvc_repo):
+    n_trials = 5
+    metric_name = "custom_name"
+    callback = DVCLiveCallback(metric_name=metric_name)
+    study = optuna.create_study()
+
+    study.optimize(objective, n_trials=n_trials, callbacks=[callback])
+
+    assert mocked_dvc_repo.experiments.save.call_count == n_trials
+
+    metrics = parse_json("dvclive-optuna/metrics.json")
+    assert metric_name in metrics
+    params = load_yaml("dvclive-optuna/params.yaml")
+    assert "x" in params
