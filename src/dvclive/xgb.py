ce540317627f1ea263cf52d3aef3e5a7de4ae791
Sigurd Spieckermann <sigurd.spieckermann@gmail.com>
2023-05-27 21:28:52 +0200

    xgb: add support for multiple metrics data sets

--- src/dvclive/xgb.py
+++ src/dvclive/xgb.py
@@ -1,5 +1,5 @@
 # ruff: noqa: ARG002
-from typing import Optional
+from typing import Optional, Sequence, Union
 
 from xgboost.callback import TrainingCallback
 
@@ -8,18 +8,29 @@ from dvclive import Live
 
 class DVCLiveCallback(TrainingCallback):
     def __init__(
-        self, metric_data, model_file=None, live: Optional[Live] = None, **kwargs
+        self,
+        metric_data: Union[str, Sequence[str]],
+        model_file=None,
+        live: Optional[Live] = None,
+        **kwargs,
     ):
         super().__init__()
-        self._metric_data = metric_data
+        if isinstance(metric_data, str):
+            metric_data = (metric_data,)
+        self._metric_spec = {
+            name: name if len(metric_data) > 1 else "" for name in metric_data
+        }
         self.model_file = model_file
         self.live = live if live is not None else Live(**kwargs)
 
     def after_iteration(self, model, epoch, evals_log):
-        for key, values in evals_log[self._metric_data].items():
-            if values:
-                latest_metric = values[-1]
-            self.live.log_metric(key, latest_metric)
+        for name, subdir in self._metric_spec.items():
+            for key, values in evals_log[name].items():
+                if values:
+                    latest_metric = values[-1]
+                self.live.log_metric(
+                    f"{subdir}/{key}" if subdir else key, latest_metric
+                )
         if self.model_file:
             model.save_model(self.model_file)
         self.live.next_step()
