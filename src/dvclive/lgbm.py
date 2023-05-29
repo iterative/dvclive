02d6b733af149cfaf77fe29c27be411cb58de932
daavoo <daviddelaiglesiacastro@gmail.com>
2023-05-29 16:51:00 +0200

    lgbm: Use `multi_eval` for backwards compatibility.

--- src/dvclive/lgbm.py
+++ src/dvclive/lgbm.py
@@ -10,9 +10,12 @@ class DVCLiveCallback:
         self.live = live if live is not None else Live(**kwargs)
 
     def __call__(self, env):
+        multi_eval = len(env.evaluation_result_list) > 1
         for eval_result in env.evaluation_result_list:
             data_name, eval_name, result = eval_result[:3]
-            self.live.log_metric(f"{data_name}-{eval_name}", result)
+            self.live.log_metric(
+                f"{data_name}/{eval_name}" if multi_eval else eval_name, result
+            )
 
         if self.model_file:
             env.model.save_model(self.model_file)
