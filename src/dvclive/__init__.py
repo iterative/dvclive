ac868e2003d05729be1a40e04ca66e0611d9ffe1
Ivan Shcheklein <shcheklein@gmail.com>
2023-05-28 18:32:10 -0700

    chore(setup): migrate to pyproject, add version

--- src/dvclive/__init__.py
+++ src/dvclive/__init__.py
@@ -1,1 +1,3 @@
+from dvclive.version import __version__, version_tuple  # noqa: F401
+
 from .live import Live  # noqa: F401
---
+++ src/dvclive/version.py
@@ -0,0 +1,7 @@
+# pylint: disable=unused-import
+try:
+    from ._dvclive_version import version as __version__  # type: ignore[import]
+    from ._dvclive_version import version_tuple  # type: ignore[import]
+except ImportError:
+    __version__ = "UNKNOWN"
+    version_tuple = (0, 0, __version__)  # type: ignore[assignment]
