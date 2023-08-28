07a64b125fc1e009cac1c4710bdbe6b17bd8b60f
dberenbaum <dave@iterative.ai>
2023-08-28 11:47:40 -0400

    revert cleanup changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -158,8 +158,12 @@ def test_nested_logging(tmp_dir):
     "html",
     [True, False],
 )
-def test_cleanup(tmp_dir, html):
-    dvclive = Live("logs", report="html" if html else None, dvcyaml="dvc.yaml")
+@pytest.mark.parametrize(
+    "dvcyaml",
+    ["dvc.yaml", "logs/dvc.yaml"],
+)
+def test_cleanup(tmp_dir, html, dvcyaml):
+    dvclive = Live("logs", report="html" if html else None, dvcyaml=dvcyaml)
     dvclive.log_metric("m1", 1)
     dvclive.next_step()
 
@@ -168,6 +172,7 @@ def test_cleanup(tmp_dir, html):
         html_path.touch()
 
     (tmp_dir / "logs" / "some_user_file.txt").touch()
+    (tmp_dir / "dvc.yaml").touch()
 
     assert (tmp_dir / dvclive.plots_dir / Metric.subfolder / "m1.tsv").is_file()
     assert (tmp_dir / dvclive.metrics_file).is_file()
@@ -176,9 +181,13 @@ def test_cleanup(tmp_dir, html):
 
     dvclive = Live("logs")
 
-    assert not (tmp_dir / "logs" / "some_user_file.txt").is_file()
+    assert (tmp_dir / "logs" / "some_user_file.txt").is_file()
     assert not (tmp_dir / dvclive.plots_dir / Metric.subfolder).exists()
     assert not (tmp_dir / dvclive.metrics_file).is_file()
+    if dvcyaml == "dvc.yaml":
+        assert (tmp_dir / dvcyaml).is_file()
+    if dvcyaml == "logs/dvc.yaml":
+        assert not (tmp_dir / dvcyaml).is_file()
     assert not (html_path).is_file()
 
 
