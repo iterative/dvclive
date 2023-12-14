c94c34f084feb2915c04852101a10d74d5bf9184
Mikhail Rozhkov <mnrozhkov@gmail.com>
2023-12-14 16:36:44 +0100

    Parametrize tests for convert_datapoints_to_list_of_dicts()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,29 +20,28 @@ def test_standardize_metric_name(framework, logged, standardized):
     assert standardize_metric_name(logged, framework) == standardized
 
 
-class TestConvertDatapointsToListOfDicts:
-    def test_dataframe(self):
-        df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
-        expected_output = [{"A": 1, "B": 3}, {"A": 2, "B": 4}]
-        assert convert_datapoints_to_list_of_dicts(df) == expected_output
+# Tests for convert_datapoints_to_list_of_dicts()
+@pytest.mark.parametrize(
+    ("input_data", "expected_output"),
+    [
+        (
+            pd.DataFrame({"A": [1, 2], "B": [3, 4]}),
+            [{"A": 1, "B": 3}, {"A": 2, "B": 4}],
+        ),
+        (np.array([[1, 3], [2, 4]]), [{0: 1, 1: 3}, {0: 2, 1: 4}]),
+        (
+            np.array([(1, 3), (2, 4)], dtype=[("A", "i4"), ("B", "i4")]),
+            [{"A": 1, "B": 3}, {"A": 2, "B": 4}],
+        ),
+        ([{"A": 1, "B": 3}, {"A": 2, "B": 4}], [{"A": 1, "B": 3}, {"A": 2, "B": 4}]),
+    ],
+)
+def test_convert_datapoints_to_list_of_dicts(input_data, expected_output):
+    assert convert_datapoints_to_list_of_dicts(input_data) == expected_output
 
-    def test_ndarray(self):
-        arr = np.array([[1, 3], [2, 4]])
-        expected_output = [{0: 1, 1: 3}, {0: 2, 1: 4}]
-        assert convert_datapoints_to_list_of_dicts(arr) == expected_output
 
-    def test_structured_array(self):
-        dtype = [("A", "i4"), ("B", "i4")]
-        structured_array = np.array([(1, 3), (2, 4)], dtype=dtype)
-        expected_output = [{"A": 1, "B": 3}, {"A": 2, "B": 4}]
-        assert convert_datapoints_to_list_of_dicts(structured_array) == expected_output
+def test_unsupported_format():
+    with pytest.raises(InvalidDataTypeError) as exc_info:
+        convert_datapoints_to_list_of_dicts("unsupported data format")
 
-    def test_list_of_dicts(self):
-        list_of_dicts = [{"A": 1, "B": 3}, {"A": 2, "B": 4}]
-        assert convert_datapoints_to_list_of_dicts(list_of_dicts) == list_of_dicts
-
-    def test_unsupported_format(self):
-        with pytest.raises(InvalidDataTypeError) as exc_info:
-            convert_datapoints_to_list_of_dicts("unsupported data format")
-
-        assert "not supported type" in str(exc_info.value)
+    assert "not supported type" in str(exc_info.value)
